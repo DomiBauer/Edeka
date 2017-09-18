2e053584e6906ac77d2735d7eef6c2f4812b4c73
DomiBauer <Dominik1.Bauer@stud.uni-regensburg.de>
2017-09-18 11:53:53 +0200

    Added Header and Changed Background

--- Edeka/app/src/main/java/com/example/dominikbauer/edeka/MainActivity.java
+++ Edeka/app/src/main/java/com/example/dominikbauer/edeka/MainActivity.java
@@ -9,8 +9,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -18,16 +16,12 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_einkaufszettel:
-                    mTextMessage.setText(R.string.title_einkaufszettel);
                     return true;
                 case R.id.navigation_produktsuche:
-                    mTextMessage.setText(R.string.title_produktsuche);
                     return true;
                 case R.id.navigation_angebote:
-                    mTextMessage.setText(R.string.title_angebote);
                     return true;
                 case R.id.navigation_ueber_uns:
-                    mTextMessage.setText(R.string.title_ueber_uns);
                     return true;
             }
             return false;
@@ -40,9 +34,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        getSupportActionBar().hide();
     }
 
 }
