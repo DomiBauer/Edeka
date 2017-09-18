2f73f895a6c43bddeabc29bb8eaeb07df4e944da
DomiBauer <Dominik1.Bauer@stud.uni-regensburg.de>
2017-09-18 15:56:06 +0200

    Added Navigation to all Pages

--- Edeka/app/src/main/java/com/example/dominikbauer/edeka/MainActivity.java
+++ Edeka/app/src/main/java/com/example/dominikbauer/edeka/MainActivity.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewStub;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,12 +20,16 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_einkaufszettel:
+                    openShoppingList();
                     return true;
                 case R.id.navigation_produktsuche:
+                    productSearch();
                     return true;
                 case R.id.navigation_angebote:
+                    openDiscount();
                     return true;
                 case R.id.navigation_ueber_uns:
+                    openAbout();
                     return true;
             }
             return false;
@@ -37,7 +45,34 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        openShoppingList();
+
         getSupportActionBar().hide();
+    }
+
+    public void openShoppingList () {
+        ViewGroup inclusionViewGroup = (ViewGroup)findViewById(R.id.content);
+        View shoppingListContent = LayoutInflater.from(this).inflate(R.layout.content_shopping_list, null);
+        inclusionViewGroup.addView(shoppingListContent);
+    }
+
+    public void productSearch () {
+        ViewGroup inclusionViewGroup = (ViewGroup)findViewById(R.id.content);
+        View shoppingListContent = LayoutInflater.from(this).inflate(R.layout.content_product_search, null);
+        inclusionViewGroup.addView(shoppingListContent);
+    }
+
+    public void openDiscount () {
+        ViewGroup inclusionViewGroup = (ViewGroup)findViewById(R.id.content);
+        View shoppingListContent = LayoutInflater.from(this).inflate(R.layout.content_discount, null);
+        inclusionViewGroup.addView(shoppingListContent);
+    }
+
+    public void openAbout () {
+        ViewGroup inclusionViewGroup = (ViewGroup)findViewById(R.id.content);
+        View aboutContent = LayoutInflater.from(this).inflate(R.layout.content_about, null);
+        inclusionViewGroup.addView(aboutContent);
+
     }
 
 }
