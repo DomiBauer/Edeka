774a7c19b643ac5ca0ce8fa7e9be96a5dd7626cc
DomiBauer <Dominik1.Bauer@stud.uni-regensburg.de>
2017-09-19 13:18:13 +0200

    Setup Map & Scroll-Bug Fix

--- Edeka/app/src/main/java/com/example/dominikbauer/edeka/MainActivity.java
+++ Edeka/app/src/main/java/com/example/dominikbauer/edeka/MainActivity.java
@@ -1,19 +1,35 @@
 package com.example.dominikbauer.edeka;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewStub;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -79,6 +95,9 @@ public class MainActivity extends AppCompatActivity {
         View aboutContent = LayoutInflater.from(this).inflate(R.layout.content_about, null);
         inclusionViewGroup.addView(aboutContent);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     public void clearContent () {
@@ -86,4 +105,45 @@ public class MainActivity extends AppCompatActivity {
         content.removeAllViews();
     }
 
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng storeLocation = new LatLng(48.393171, 12.605773);
+        mMap.addMarker(new MarkerOptions().position(storeLocation).title("EDEKA Steinberger"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(storeLocation, 15));
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        setUpMapScrolling();
+    }
+
+    private void setUpMapScrolling () {
+        final ScrollView scroll = (ScrollView) findViewById(R.id.scrollView);
+        ImageView transparent = (ImageView)findViewById(R.id.imagetrans);
+
+        transparent.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Disallow ScrollView to intercept touch events.
+                        scroll.requestDisallowInterceptTouchEvent(true);
+                        // Disable touch on transparent view
+                        return false;
+
+                    case MotionEvent.ACTION_UP:
+                        // Allow ScrollView to intercept touch events.
+                        scroll.requestDisallowInterceptTouchEvent(false);
+                        return true;
+
+                    case MotionEvent.ACTION_MOVE:
+                        scroll.requestDisallowInterceptTouchEvent(true);
+                        return false;
+
+                    default:
+                        return true;
+                }
+            }
+        });
+    }
 }
